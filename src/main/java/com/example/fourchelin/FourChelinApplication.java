422818699212cf299dd1a4880ba659ac03655b53
shinheekim <shinhee1241@gmail.com>
2024-11-22 19:24:55 +0900

    init commit

---
+++ src/main/java/com/example/fourchelin/FourChelinApplication.java
@@ -0,0 +1,13 @@
+package com.example.fourchelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourChelinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourChelinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fourchelin/FourChelinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fourchelin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourChelinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
