f012e19932c801c15226a3fbab1fba53766f43f9
TIP_Santa <bhb89@naver.com>
2024-11-25 14:46:02 +0900

    [#8] Feat: SearchHistory, PopularSearch Entity 생성 및 구현

---
+++ src/main/java/com/example/fourchelin/domain/search/entity/SearchHistory.java
@@ -0,0 +1,25 @@
+package com.example.fourchelin.domain.search.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SearchHistory{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String keyword;
+    @Column(nullable = false)
+    private LocalDateTime searchDateTime;
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+}
