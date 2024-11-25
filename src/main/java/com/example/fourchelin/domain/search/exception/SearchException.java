9914c98bc69611f221a2e1785809e8633ec23fe3
nurigaon96 <nurigaon96@gmail.com>
2024-11-25 16:41:59 +0900

    [#5] test: 잘못 작성된 Test 클래스 삭제

--- src/main/java/com/example/fourchelin/domain/search/exception/SearchException.java
+++
@@ -1,8 +0,0 @@
-package com.example.fourchelin.domain.search.exception;
-
-public class SearchException extends RuntimeException {
-
-    public SearchException(String message) {
-        super(message);
-    }
-}
