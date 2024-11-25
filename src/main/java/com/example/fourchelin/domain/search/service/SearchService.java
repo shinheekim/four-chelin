c828b0421085e416cc09d0ca4383aba49a2295fb
TIP_Santa <bhb89@naver.com>
2024-11-25 19:08:15 +0900

    [#15] Feat: SearchService - 검색 시 검색기록 저장기능 추가

--- src/main/java/com/example/fourchelin/domain/search/service/SearchService.java
+++ src/main/java/com/example/fourchelin/domain/search/service/SearchService.java
@@ -1,5 +1,7 @@
 package com.example.fourchelin.domain.search.service;
 
+import com.example.fourchelin.domain.search.entity.SearchHistory;
+import com.example.fourchelin.domain.search.repository.SearchHistoryRepository;
 import com.example.fourchelin.domain.store.dto.response.StorePageResponse;
 import com.example.fourchelin.domain.store.entity.Store;
 import com.example.fourchelin.domain.store.exception.StoreException;
@@ -10,21 +12,33 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class SearchService {
 
     private final StoreRepository storeRepository;
+    private final SearchHistoryRepository searchHistoryRepository;
 
     public StorePageResponse searchStore(String keyword, int page, int size) {
         if (keyword == null || keyword.trim().isEmpty()) {
             throw new StoreException("검색어를 입력해주세요");
         }
 
+        saveSearchHistory(keyword);
+
         Pageable pageable = PageRequest.of(page - 1, size);
         Page<Store> stores = storeRepository.findByKeyword(keyword, pageable);
 
         return new StorePageResponse(stores);
-
+    }
+    
+    private void saveSearchHistory(String keyword) {
+        SearchHistory searchHistory = SearchHistory.builder()
+                .keyword(keyword)
+                .searchDateTime(LocalDateTime.now())
+                .build();
+        searchHistoryRepository.save(searchHistory);
     }
 }
