ca0d08bde67bb3eaff4863e150f3f7bbb0db87a2
TIP_Santa <bhb89@naver.com>
2024-11-26 10:06:50 +0900

    [#15] Feat: SearchHistoryRepositoryCustom, SearchHistoryRepositoryCustomImpl - 검색목록을 볼 수 있는 기능 추가

--- src/main/java/com/example/fourchelin/domain/search/repository/support/SearchHistoryRepositoryCustom.java
+++ src/main/java/com/example/fourchelin/domain/search/repository/support/SearchHistoryRepositoryCustom.java
@@ -3,6 +3,9 @@ package com.example.fourchelin.domain.search.repository.support;
 import com.example.fourchelin.domain.member.entity.Member;
 import com.example.fourchelin.domain.search.entity.SearchHistory;
 
+import java.util.List;
+
 public interface SearchHistoryRepositoryCustom {
     SearchHistory findByKeywordAndMember(String keyword, Member member);
+    List<SearchHistory> keywordFindByMember(Member member);
 }
--- src/main/java/com/example/fourchelin/domain/search/repository/support/SearchHistoryRepositoryCustomImpl.java
+++ src/main/java/com/example/fourchelin/domain/search/repository/support/SearchHistoryRepositoryCustomImpl.java
@@ -7,21 +7,31 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class SearchHistoryRepositoryCustomImpl implements SearchHistoryRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
+    private final QSearchHistory searchHistory = QSearchHistory.searchHistory;
 
     @Override
     public SearchHistory findByKeywordAndMember(String keyword, Member member) {
-        QSearchHistory searchHistory = QSearchHistory.searchHistory;
-
         return jpaQueryFactory
                 .selectFrom(searchHistory)
                 .where(searchHistory.keyword.eq(keyword)
                         .and(searchHistory.member.eq(member))
                 )
                 .fetchOne();
+    }
+    @Override
+    public List<SearchHistory> keywordFindByMember(Member member) {
+        return jpaQueryFactory
+                .selectFrom(searchHistory)
+                .where(searchHistory.member.eq(member))
+                .orderBy(searchHistory.searchDateTime.desc())
+                .limit(10)
+                .fetch();
     }
 }
