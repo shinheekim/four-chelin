579339815c810948abbf87f52d709738ccf1727d
TIP_Santa <bhb89@naver.com>
2024-11-26 21:41:41 +0900

    [#25] Feat: PopularKeywordRepositoryCustom, Impl - 인기검색어 조회 기능 추가

--- src/main/java/com/example/fourchelin/domain/search/repository/support/PopularKeywordRepositoryCustom.java
+++ src/main/java/com/example/fourchelin/domain/search/repository/support/PopularKeywordRepositoryCustom.java
@@ -3,8 +3,10 @@ package com.example.fourchelin.domain.search.repository.support;
 import com.example.fourchelin.domain.search.entity.PopularKeyword;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface PopularKeywordRepositoryCustom {
     Optional<PopularKeyword> findByKeywordAndTrendDate(String keyword, LocalDate trendDate);
+    List<String> findTop10PopularKeywords(LocalDate sevenDaysAgo);
 }
