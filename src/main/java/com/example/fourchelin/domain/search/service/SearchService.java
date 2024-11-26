57146d4713857f2cd88d6267c5de936e3cb74981
TIP_Santa <bhb89@naver.com>
2024-11-26 10:05:54 +0900

    [#15] Feat: SearchService - 검색목록을 볼 수 있는 기능 추가

--- src/main/java/com/example/fourchelin/domain/search/service/SearchService.java
+++ src/main/java/com/example/fourchelin/domain/search/service/SearchService.java
@@ -4,6 +4,7 @@ import com.example.fourchelin.domain.member.entity.Member;
 import com.example.fourchelin.domain.search.entity.SearchHistory;
 import com.example.fourchelin.domain.search.repository.SearchHistoryRepository;
 import com.example.fourchelin.domain.store.dto.response.StorePageResponse;
+import com.example.fourchelin.domain.store.dto.response.StoreResponse;
 import com.example.fourchelin.domain.store.entity.Store;
 import com.example.fourchelin.domain.store.exception.StoreException;
 import com.example.fourchelin.domain.store.repository.StoreRepository;
@@ -14,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -36,6 +38,21 @@ public class SearchService {
         Page<Store> stores = storeRepository.findByKeyword(keyword, pageable);
 
         return new StorePageResponse(stores);
+    }
+
+    public List<String> searchKeywordStore(Member member) {
+        // 인증 유저가 아닌 경우
+        if(member == null) {
+            return List.of();
+        }
+        List<SearchHistory> searchHistories = searchHistoryRepository.keywordFindByMember(member);
+        // 검색 결과가 없는 경우
+        if (searchHistories.isEmpty()) {
+            return List.of();
+        }
+        return searchHistories.stream()
+                .map(SearchHistory::getKeyword)
+                .toList();
     }
 
     private void saveOrUpdateSearchHistory(String keyword, Member member) {
