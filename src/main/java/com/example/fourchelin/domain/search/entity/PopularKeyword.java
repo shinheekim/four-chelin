28f5ac9edd3e3fa3fbe0486374471b38954dc3d9
TIP_Santa <bhb89@naver.com>
2024-11-26 20:53:19 +0900

    [#16] Feat: PopularKeyword - 인기검색어 저장 및 카운팅 기능 추가

--- src/main/java/com/example/fourchelin/domain/search/entity/PopularKeyword.java
+++ src/main/java/com/example/fourchelin/domain/search/entity/PopularKeyword.java
@@ -6,18 +6,29 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class PopularSearch {
+public class PopularKeyword {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
-    private String popularKeyword;
+    @Column(nullable = false, unique = true)
+    private String keyword;
     @Column(nullable = false)
     private Long searchCount;
     @Column(nullable = false)
     private LocalDate trendDate;
+
+    public PopularKeyword(String keyword, LocalDate trendDate) {
+        this.keyword = keyword;
+        this.trendDate = trendDate;
+        this.searchCount = 1L;
+    }
+
+    public void incrementCount() {
+        this.searchCount++;
+    }
 }
