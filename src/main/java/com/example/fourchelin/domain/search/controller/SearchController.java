bc76a222357ff7b66cff812b64bef36d92f7b0e7
TIP_Santa <bhb89@naver.com>
2024-11-26 10:04:49 +0900

    [#15] Feat: SearchController - 검색목록을 볼 수 있는 기능 추가

--- src/main/java/com/example/fourchelin/domain/search/controller/SearchController.java
+++ src/main/java/com/example/fourchelin/domain/search/controller/SearchController.java
@@ -5,6 +5,7 @@ import com.example.fourchelin.common.template.RspTemplate;
 import com.example.fourchelin.domain.member.entity.Member;
 import com.example.fourchelin.domain.search.service.SearchService;
 import com.example.fourchelin.domain.store.dto.response.StorePageResponse;
+import com.example.fourchelin.domain.store.dto.response.StoreResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +14,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/searches")
 @RequiredArgsConstructor
 public class SearchController {
 
     private final SearchService searchService;
+
+    @GetMapping
+    public RspTemplate<List<String>> searchKeywordStore(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        // 인증된 유저일 경우 member 객체 가져오고 아니면 null
+        Member member = (userDetails != null) ? userDetails.getMember() : null;
+        List<String> res = searchService.searchKeywordStore(member);
+        return new RspTemplate<>(HttpStatus.OK, res);
+    }
 
     @GetMapping("/v1/stores")
     public RspTemplate<StorePageResponse> searchStore(@RequestParam String keyword,
