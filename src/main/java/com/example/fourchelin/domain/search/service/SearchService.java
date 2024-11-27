37dc92d613b82a7561b33d67c5a99d41863582fc
TIP_Santa <bhb89@naver.com>
2024-11-27 20:23:39 +0900

    [#27] Feat: SearchService - v2_In-Memory Cache 를 사용한 인기검색어 저장 기능 추가

--- src/main/java/com/example/fourchelin/domain/search/service/SearchService.java
+++ src/main/java/com/example/fourchelin/domain/search/service/SearchService.java
@@ -2,6 +2,7 @@ package com.example.fourchelin.domain.search.service;
 
 import com.example.fourchelin.domain.member.entity.Member;
 import com.example.fourchelin.domain.search.entity.PopularKeyword;
+import com.example.fourchelin.domain.search.entity.PopularKeywordCache;
 import com.example.fourchelin.domain.search.entity.SearchHistory;
 import com.example.fourchelin.domain.search.repository.PopularKeywordRepository;
 import com.example.fourchelin.domain.search.repository.SearchHistoryRepository;
@@ -22,6 +23,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -30,25 +32,38 @@ public class SearchService {
     private final StoreRepository storeRepository;
     private final SearchHistoryRepository searchHistoryRepository;
     private final PopularKeywordRepository popularKeywordRepository;
+    private final Map<String, PopularKeywordCache> popularKeywordCache = new ConcurrentHashMap<>();
 
     @Transactional
     public StorePageResponse searchStore(String keyword, int page, int size, Member member) {
-        if (keyword == null || keyword.trim().isEmpty()) {
-            throw new StoreException("검색어를 입력해주세요");
-        }
-        // 인증된 사용자인 경우에만 검색기록 저장
-        if (member != null) {
-            saveOrUpdateSearchHistory(keyword, member);
-        }
+        searchKeywordAndUpdateSearchHistory(keyword, member);
         saveOrUpdatePopularKeyword(keyword);
         Pageable pageable = PageRequest.of(page - 1, size);
         Page<Store> stores = storeRepository.findByKeyword(keyword, pageable);
         return new StorePageResponse(stores);
     }
 
+    // In-memory Cache
+    @Transactional
+    public StorePageResponse searchStoreV2(String keyword, int page, int size, Member member) {
+        searchKeywordAndUpdateSearchHistory(keyword, member);
+        updateKeywordCount(keyword);
+        Pageable pageable = PageRequest.of(page - 1, size);
+        Page<Store> stores = storeRepository.findByKeyword(keyword, pageable);
+        return new StorePageResponse(stores);
+    }
+
+    public void searchKeywordAndUpdateSearchHistory(String keyword, Member member) {
+        if (keyword == null || keyword.trim().isEmpty()) {
+            throw new StoreException("검색어를 입력해주세요");
+        }
+        if (member != null) {
+            saveOrUpdateSearchHistory(keyword, member);
+        }
+    }
+
     public Map<String, List<String>> searchKeyword(Member member) {
         Map<String, List<String>> result = new HashMap<>();
-        // 사용자 검색 기록 조회
         if (member != null) {
             List<SearchHistory> searchHistories = searchHistoryRepository.keywordFindByMember(member);
             List<String> searchKeywords = searchHistories.stream()
@@ -85,14 +100,32 @@ public class SearchService {
         Optional<PopularKeyword> optionalPopularKeyword = popularKeywordRepository.findByKeywordAndTrendDate(keyword, today);
 
         if (optionalPopularKeyword.isPresent()) {
-            // 기존 키워드가 있는 경우 카운트를 증가시킴
             PopularKeyword popularKeyword = optionalPopularKeyword.get();
             popularKeyword.incrementCount();
             popularKeywordRepository.save(popularKeyword);
         } else {
-            // 키워드가 없는 경우 새로 생성하고 카운트를 1로 설정
             PopularKeyword newPopularKeyword = new PopularKeyword(keyword, today);
             popularKeywordRepository.save(newPopularKeyword);
         }
+    }
+
+    public void updateKeywordCount(String keyword) {
+        LocalDate today = LocalDate.now();
+        String cacheKey = createCacheKey(keyword, today);
+        PopularKeywordCache keywordCache = popularKeywordCache.get(cacheKey);
+        if (keywordCache != null) {
+            keywordCache.incrementCount();
+        } else {
+            popularKeywordCache.put(cacheKey, new PopularKeywordCache(keyword, today, 1L));
+        }
+    }
+
+    private String createCacheKey(String keyword, LocalDate date) {
+        return keyword + "_" + date.toString();
+    }
+
+    // 테스트 결과 출력을 위한 코드
+    public Map<String, PopularKeywordCache> getPopularKeywordCache() {
+        return popularKeywordCache;
     }
 }
