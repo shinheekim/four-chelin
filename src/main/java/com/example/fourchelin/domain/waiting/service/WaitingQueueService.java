d7417dc64f53ca99449634fb2f19c25c86603ae7
shinheekim <shinhee1241@gmail.com>
2024-11-27 10:26:28 +0900

    [#9] Chore: 불필요한 주석 처리 제거

--- src/main/java/com/example/fourchelin/domain/waiting/service/WaitingQueueService.java
+++ src/main/java/com/example/fourchelin/domain/waiting/service/WaitingQueueService.java
@@ -7,29 +7,24 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 @Service
 public class WaitingQueueService {
-    private final Queue<Long> waitingQueue = new ConcurrentLinkedQueue<>();  // 스레드 안정성
+    private final Queue<Long> waitingQueue = new ConcurrentLinkedQueue<>();
 
-    // 대기열에 추가
     public void addToQueue(Long memberId) {
         waitingQueue.offer(memberId);
     }
 
-    // 대기열에서 요소 제거
     public boolean removeFromQueue(Long memberId) {
         return waitingQueue.remove(memberId);
     }
 
-    // 대기열 앞쪽 통과
     public Long pollFromQueue() {
         return waitingQueue.poll();
     }
 
-    // 대기열 전체 크기 조회
     public int sizeOfQueue() {
         return waitingQueue.size();
     }
 
-    // 대기열 초기화
     public void clearQueue() {
         waitingQueue.clear();
     }
